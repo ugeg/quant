d4dc84c87dfe194a51df42eeb68dfb06ca78b945
ugeg <ugeg@outlook.com>
2020-09-26 23:30:52 +0800

    策略

---
+++ UserStrategy/Strategy.py
@@ -0,0 +1,149 @@
+# -*- coding: utf-8 -*-
+# @Author  : pengj    <ugeg@163.com>
+# @Time    : 2019/6/29 12:18
+# @File    : Strategy.py
+from pyalgotrade import strategy
+from pyalgotrade.technical import vwap, bollinger, ma, rsi, cross
+
+
+# 交易量加权平均价格（VWAP）动量策略
+class VWAPMomentum(strategy.BacktestingStrategy):
+    def __init__(self, feed, instrument, vwapWindowSize, threshold):
+        strategy.BacktestingStrategy.__init__(self, feed)
+        self.__instrument = instrument
+        self.__vwap = vwap.VWAP(feed[instrument], vwapWindowSize)
+        self.__threshold = threshold
+
+    def getVWAP(self):
+        return self.__vwap
+
+    def onBars(self, bars):
+        vwap = self.__vwap[-1]
+        if vwap is None:
+            return
+
+        shares = self.getBroker().getShares(self.__instrument)
+        price = bars[self.__instrument].getClose()
+        notional = shares * price
+
+        if price > vwap * (1 + self.__threshold) and notional < 1000000:
+            self.marketOrder(self.__instrument, 100)
+        elif price < vwap * (1 - self.__threshold) and notional > 0:
+            self.marketOrder(self.__instrument, -100)
+    # def plot(self):
+    #     plt = plotter.StrategyPlotter(strat, True, False, True)
+    #     plt.getInstrumentSubplot(instrument).addDataSeries("vwap", strat.getVWAP())
+
+
+# 布林带策略
+class BBands(strategy.BacktestingStrategy):
+    def __init__(self, feed, instrument, bBandsPeriod):
+        strategy.BacktestingStrategy.__init__(self, feed)
+        self.__instrument = instrument
+        self.__bbands = bollinger.BollingerBands(feed[instrument].getCloseDataSeries(), bBandsPeriod, 2)
+
+    def getBollingerBands(self):
+        return self.__bbands
+
+    def onBars(self, bars):
+        lower = self.__bbands.getLowerBand()[-1]
+        upper = self.__bbands.getUpperBand()[-1]
+        if lower is None:
+            return
+
+        shares = self.getBroker().getShares(self.__instrument)
+        bar = bars[self.__instrument]
+        if shares == 0 and bar.getClose() < lower:
+            sharesToBuy = int(self.getBroker().getCash(False) / bar.getClose())
+            self.marketOrder(self.__instrument, sharesToBuy)
+        elif shares > 0 and bar.getClose() > upper:
+            self.marketOrder(self.__instrument, -1 * shares)
+    # def plot(self):
+    #     plt = plotter.StrategyPlotter(strat, True, True, True)
+    #     plt.getInstrumentSubplot(cname).addDataSeries("upper", strat.getBollingerBands().getUpperBand())
+    #     plt.getInstrumentSubplot(cname).addDataSeries("middle", strat.getBollingerBands().getMiddleBand())
+    #     plt.getInstrumentSubplot(cname).addDataSeries("lower", strat.getBollingerBands().getLowerBand())
+#RSI2策略
+class RSI2(strategy.BacktestingStrategy):
+    def __init__(self, feed, instrument, entrySMA, exitSMA, rsiPeriod, overBoughtThreshold, overSoldThreshold):
+        strategy.BacktestingStrategy.__init__(self, feed)
+        self.__instrument = instrument
+        # We'll use adjusted close values, if available, instead of regular close values.
+        if feed.barsHaveAdjClose():
+            self.setUseAdjustedValues(True)
+        self.__priceDS = feed[instrument].getPriceDataSeries()
+        self.__entrySMA = ma.SMA(self.__priceDS, entrySMA)
+        self.__exitSMA = ma.SMA(self.__priceDS, exitSMA)
+        self.__rsi = rsi.RSI(self.__priceDS, rsiPeriod)
+        self.__overBoughtThreshold = overBoughtThreshold
+        self.__overSoldThreshold = overSoldThreshold
+        self.__longPos = None
+        self.__shortPos = None
+
+    def getEntrySMA(self):
+        return self.__entrySMA
+
+    def getExitSMA(self):
+        return self.__exitSMA
+
+    def getRSI(self):
+        return self.__rsi
+
+    def onEnterCanceled(self, position):
+        if self.__longPos == position:
+            self.__longPos = None
+        elif self.__shortPos == position:
+            self.__shortPos = None
+        else:
+            assert(False)
+
+    def onExitOk(self, position):
+        if self.__longPos == position:
+            self.__longPos = None
+        elif self.__shortPos == position:
+            self.__shortPos = None
+        else:
+            assert(False)
+
+    def onExitCanceled(self, position):
+        # If the exit was canceled, re-submit it.
+        position.exitMarket()
+
+    def onBars(self, bars):
+        # Wait for enough bars to be available to calculate SMA and RSI.
+        if self.__exitSMA[-1] is None or self.__entrySMA[-1] is None or self.__rsi[-1] is None:
+            return
+
+        bar = bars[self.__instrument]
+        if self.__longPos is not None:
+            if self.exitLongSignal():
+                self.__longPos.exitMarket()
+        elif self.__shortPos is not None:
+            if self.exitShortSignal():
+                self.__shortPos.exitMarket()
+        else:
+            if self.enterLongSignal(bar):
+                shares = int(self.getBroker().getCash() * 0.9 / bars[self.__instrument].getPrice())
+                self.__longPos = self.enterLong(self.__instrument, shares, True)
+            elif self.enterShortSignal(bar):
+                shares = int(self.getBroker().getCash() * 0.9 / bars[self.__instrument].getPrice())
+                self.__shortPos = self.enterShort(self.__instrument, shares, True)
+
+    def enterLongSignal(self, bar):
+        return bar.getPrice() > self.__entrySMA[-1] and self.__rsi[-1] <= self.__overSoldThreshold
+
+    def exitLongSignal(self):
+        return cross.cross_above(self.__priceDS, self.__exitSMA) and not self.__longPos.exitActive()
+
+    def enterShortSignal(self, bar):
+        return bar.getPrice() < self.__entrySMA[-1] and self.__rsi[-1] >= self.__overBoughtThreshold
+
+    def exitShortSignal(self):
+        return cross.cross_below(self.__priceDS, self.__exitSMA) and not self.__shortPos.exitActive()
+    # def plot(self):
+    #     plt = plotter.StrategyPlotter(strat, True, False, True)
+    #     plt.getInstrumentSubplot(instrument).addDataSeries("Entry SMA", strat.getEntrySMA())
+    #     plt.getInstrumentSubplot(instrument).addDataSeries("Exit SMA", strat.getExitSMA())
+    #     plt.getOrCreateSubplot("rsi").addDataSeries("RSI", strat.getRSI())
+    #     plt.getOrCreateSubplot("rsi").addLine("Overbought", overBoughtThreshold)
+    #     plt.getOrCreateSubplot("rsi").addLine("Oversold", overSoldThreshold)
---
+++ UserStrategy/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# @Author  : pengj    <ugeg@163.com>
+# @Time    : 2019/6/29 12:16
+# @File    : __init__.py.py
