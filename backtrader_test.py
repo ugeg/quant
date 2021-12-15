d0f55633f4cc931179744b793d06db1127725e59
ugeg <ugeg@outlook.com>
2021-12-16 00:05:55 +0800

    添加 PandasDirectData

--- backtrader_test.py
+++ backtrader_test.py
@@ -1,14 +1,19 @@
+import time
+from datetime import datetime
+
 import pandas as pd
 
 import utils.backtrader_util
 from utils import global_operator
-
+from matplotlib import pyplot as plt
 import backtrader as bt
-import matplotlib as plt
-#正常显示画图时出现的中文和负号
+# 正常显示画图时出现的中文和负号
 from pylab import mpl
-mpl.rcParams['font.sans-serif']=['SimHei']
-mpl.rcParams['axes.unicode_minus']=False
+
+mpl.rcParams['font.sans-serif'] = ['SimHei']
+mpl.rcParams['axes.unicode_minus'] = False
+
+
 # import matplotlib as mpl
 # mpl.rcParams['font.sans-serif'] = ['KaiTi', 'SimHei', 'FangSong']  # 汉字字体,优先使用楷体，如果找不到楷体，则使用黑体
 # mpl.rcParams['font.size'] = 12  # 字体大小
@@ -16,41 +21,82 @@ mpl.rcParams['axes.unicode_minus']=False
 # Create a Stratey
 
 class MyStrategy(utils.backtrader_util.StrategyDefault):
-    params=( ('maperiod',15),
-            ('printlog',True),)
+    params = (('maperiod', 15),
+              ('printlog', True),)
 
     def __init__(self):
-        #指定价格序列
-        self.dataclose=self.datas[0].close
+        # 指定价格序列
+        self.dataclose = self.datas[0].close
 
         # 初始化交易指令、买卖价格和手续费
         self.order = None
         self.buyprice = None
         self.buycomm = None
 
-        #添加移动均线指标
+        # 添加移动均线指标
         self.sma = bt.indicators.SimpleMovingAverage(
-                      self.datas[0], period=self.params.maperiod)
+            self.datas[0], period=self.params.maperiod)
 
-    #策略核心，根据条件执行买卖交易指令（必选）
+    # 策略核心，根据条件执行买卖交易指令（必选）
     def next(self):
         # 记录收盘价
-        #self.log(f'收盘价, {dataclose[0]}')
-        if self.order: # 检查是否有指令等待执行,
+        # self.log(f'收盘价, {dataclose[0]}')
+        if self.order:  # 检查是否有指令等待执行,
             return
         # 检查是否持仓
-        if not self.position: # 没有持仓
-            #执行买入条件判断：收盘价格上涨突破15日均线
+        if not self.position:  # 没有持仓
+            # 执行买入条件判断：收盘价格上涨突破15日均线
             if self.dataclose[0] > self.sma[0]:
                 self.log('BUY CREATE, %.2f' % self.dataclose[0])
-                #执行买入
+                # 执行买入
                 self.order = self.buy()
         else:
-            #执行卖出条件判断：收盘价格跌破15日均线
+            # 执行卖出条件判断：收盘价格跌破15日均线
             if self.dataclose[0] < self.sma[0]:
                 self.log('SELL CREATE, %.2f' % self.dataclose[0])
-                #执行卖出
+                # 执行卖出
                 self.order = self.sell()
+
+
+class MyStrategy1(utils.backtrader_util.StrategyDefault):
+    params = (('maperiod', 15),
+              ('printlog', True),)
+
+    def __init__(self):
+        # 指定价格序列
+        self.dataclose = self.datas[0].close
+
+        # 初始化交易指令、买卖价格和手续费
+        self.order = None
+        self.buyprice = None
+        self.buycomm = None
+
+        # 添加移动均线指标
+        self.sma = [bt.indicators.SimpleMovingAverage(
+            self.datas[i], period=self.params.maperiod) for i in range(len(self.datas))]
+
+    # 策略核心，根据条件执行买卖交易指令（必选）
+    def next(self):
+        # 记录收盘价
+        # self.log(f'收盘价, {dataclose[0]}')
+        if self.order:  # 检查是否有指令等待执行,
+            return
+        for i in range(len(self.datas)):
+            data = self.datas[i]
+            position = self.getpositions()[i]
+            name = data._name
+            if not position:
+                if data.close[0] > self.sma[i][0]:
+                    self.log('BUY %s CREATE, %.2f' % (name, self.dataclose[0]))
+                    # 执行买入
+                    self.order = self.buy(data)
+            else:
+                # 执行卖出条件判断：收盘价格跌破15日均线
+                if data.close[0] < self.sma[i][0]:
+                    self.log('SELL %s CREATE, %.2f' % (name, self.dataclose[0]))
+                    # 执行卖出
+                    self.order = self.sell(data)
+
 
 class TestStrategy(bt.Strategy):
 
@@ -60,10 +106,22 @@ class TestStrategy(bt.Strategy):
         print('%s, %s' % (dt.isoformat(), txt))
 
     def __init__(self):
+        self.sizer.setsizing(10)
         # Keep a reference to the "close" line in the data[0] dataseries
         self.dataclose = self.datas[0].close
         # To keep track of pending orders
         self.order = None
+        self.sma = bt.indicators.SimpleMovingAverage(
+            self.datas[0], period=10)
+        # Indicators for the plotting show
+        bt.indicators.ExponentialMovingAverage(self.datas[0], period=25)
+        bt.indicators.WeightedMovingAverage(self.datas[0], period=25,
+                                            subplot=True)
+        bt.indicators.StochasticSlow(self.datas[0])
+        bt.indicators.MACDHisto(self.datas[0])
+        rsi = bt.indicators.RSI(self.datas[0])
+        bt.indicators.SmoothedMovingAverage(rsi, period=10)
+        bt.indicators.ATR(self.datas[0], plot=False)
 
     def notify(self, order):
         if order.status in [order.Submitted, order.Accepted]:
@@ -151,11 +209,12 @@ class my_strategy1(bt.Strategy):
                 # 执行卖出
                 self.order = self.sell()
 
+
 class mySizer(bt.sizers.AllInSizerInt):
     def _getsizing(self, comminfo, cash, data, isbuy):
         position = self.broker.getposition(data)
         if not position:
-            size = cash * (self.params.percents / 100) // (data.close[0]*100)*100
+            size = cash * (self.params.percents / 100) // (data.close[0] * 100) * 100
         else:
             size = position.size
 
@@ -164,17 +223,22 @@ class mySizer(bt.sizers.AllInSizerInt):
 
         return size
 
+
 if __name__ == '__main__':
     # 初始化模型
     cerebro = bt.Cerebro()
-    cerebro.addstrategy(MyStrategy)
-    # cerebro.addstrategy(TestStrategy)
+    # Add a strategy
+    # strats = cerebro.optstrategy(
+    #     TestStrategy,
+    #     maperiod=range(10, 31))
+    # cerebro.addstrategy(MyStrategy1)
+    cerebro.addstrategy(TestStrategy)
     # cerebro.addstrategy(my_strategy1)
     # 设定初始资金
     cerebro.broker.setcash(100000.0)
     # 手续费
     # cerebro.broker.setcommission(0.0005)
-    comminfo = utils.backtrader_util.StampDutyCommission(stamp_duty=0.001,commission=0.0005)
+    comminfo = utils.backtrader_util.StampDutyCommission(stamp_duty=0.001, commission=0.0005)
     cerebro.broker.addcommissioninfo(comminfo)
     # 每次交易买入的股数
     # cerebro.addsizer(bt.sizers.FixedSize, stake=500)
@@ -183,21 +247,31 @@ if __name__ == '__main__':
     cerebro.addsizer(mySizer, percents=95)
 
     # socket_list = ['比亚迪','紫光国微']
-    socket_list = ['王府井',]
+    socket_list = ['比亚迪']
+    # socket_list = ['王府井',]
+    # 回测期间
+    start = datetime(2021, 1, 1)
+    end = datetime(2021, 12, 1)
     for socket in socket_list:
-        df = utils.backtrader_util.get_stock_daily(socket,'20200628')
-        data = bt.feeds.PandasData(dataname=df)
-        cerebro.adddata(data,name=socket)
+        # df = utils.backtrader_util.get_stock_daily_data(socket, '20200628')
+        # data = bt.feeds.PandasData(dataname=df, fromdate=start, todate=end, datetime='trade_date', open=1, high=2,
+        #                            low=3, close=4, volume=5, openinterest=-1)
+        df = utils.backtrader_util.get_stock_daily_direct_data(socket, '20200628')
+        data = bt.feeds.PandasDirectData(dataname=df, fromdate=start, todate=end, datetime=0, open=1, high=2,
+                                         low=3, close=4, volume=5, openinterest=-1)
+        cerebro.adddata(data, name=socket)
     cerebro.addanalyzer(bt.analyzers.Returns)
     # 策略执行前的资金
     print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
     results = cerebro.run(maxcpus=8)
     # 策略执行后的资金
     print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
-    cerebro.plot(style='bar',tight=False,width=160,height=90)
+    # cerebro.plot(style='bar',tight=False,width=160,height=90)
     rets = results[0].analyzers.returns.get_analysis()
     print(rets)
     # rets = results[1].analyzers.returns.get_analysis()
     # print(rets)
     # cerebro.plot(style='candlestick')
     # cerebro.plot(volume=False)
+    cerebro.plot()
+    # plt.show()
--- utils/backtrader_util.py
+++ utils/backtrader_util.py
@@ -5,8 +5,11 @@ from matplotlib import pyplot as plt
 from utils import global_operator, time_util
 
 from pylab import mpl
-mpl.rcParams['font.sans-serif']=['SimHei']
-mpl.rcParams['axes.unicode_minus']=False
+
+mpl.rcParams['font.sans-serif'] = ['SimHei']
+mpl.rcParams['axes.unicode_minus'] = False
+
+
 class StampDutyCommission(bt.CommInfoBase):
     params = (
         ("stamp_duty", 0.001),  # 印花税
@@ -14,68 +17,96 @@ class StampDutyCommission(bt.CommInfoBase):
     )
 
     def _getcommission(self, size, price, pseudoexec):
-        comm = round(size * price * self.p.commission,2)
+        comm = round(size * price * self.p.commission, 2)
         comm = max(comm, 5)
         if size > 0:
             return comm
         else:
-            duty = round(size * price * self.p.stamp_duty,2)
-            return round(-comm - duty,2)
+            duty = round(size * price * self.p.stamp_duty, 2)
+            return round(-comm - duty, 2)
+
+
 class StrategyDefault(bt.Strategy):
-    params = (('socket_name',' '),
-        ('printlog', True),)
+    params = (('socket_name', ' '),
+              ('printlog', True),)
 
     # 交易记录日志（可省略，默认不输出结果）
     def log(self, txt, dt=None, doprint=False):
         if self.params.printlog or doprint:
             dt = dt or self.datas[0].datetime.date(0)
             print(f'{dt.isoformat()},{txt}')
-    #记录交易执行情况（可省略，默认不输出结果）
+
+    # 记录交易执行情况（可省略，默认不输出结果）
     def notify_order(self, order):
         if order.status in [order.Submitted, order.Accepted]:
             return
         if order.status in [order.Completed]:
             if order.isbuy():
-                self.log(f'买入:{list(self.dnames.keys())[0]} 价格:{order.executed.price},成本:{order.executed.value},手续费:{order.executed.comm}')
+                self.log(
+                    f'买入:{list(self.dnames.keys())[0]} 价格:{order.executed.price},成本:{order.executed.value},手续费:{order.executed.comm}')
                 self.buyprice = order.executed.price
                 self.buycomm = order.executed.comm
             else:
-                self.log(f'卖出:{list(self.dnames.keys())[0]} 价格：{order.executed.price},成本: {order.executed.value},手续费{order.executed.comm}')
+                self.log(
+                    f'卖出:{list(self.dnames.keys())[0]} 价格：{order.executed.price},成本: {order.executed.value},手续费{order.executed.comm}')
             self.bar_executed = len(self)
         # 如果指令取消/交易失败, 报告结果
         elif order.status in [order.Canceled, order.Margin, order.Rejected]:
             self.log('交易失败')
         self.order = None
 
-    #记录交易收益情况（可省略，默认不输出结果）
-    def notify_trade(self,trade):
+    # 记录交易收益情况（可省略，默认不输出结果）
+    def notify_trade(self, trade):
         if trade.isclosed:
-            self.log(f'结算 {list(self.dnames.keys())[0]} 毛收益 {trade.pnl:.2f}, 净收益 {trade.pnlcomm:.2f},手续费{trade.commission:.2f}')
+            self.log(
+                f'结算 {list(self.dnames.keys())[0]} 毛收益 {trade.pnl:.2f}, 净收益 {trade.pnlcomm:.2f},手续费{trade.commission:.2f}')
 
-    #回测结束后输出结果（可省略，默认输出结果）
+    # 回测结束后输出结果（可省略，默认输出结果）
     def stop(self):
-        self.log('期末总资金 %.2f' %( self.broker.getvalue()), doprint=True)
-def get_stock_daily(code, start_date, end_date=None):
+        self.log('期末总资金 %.2f' % (self.broker.getvalue()), doprint=True)
+
+
+def get_stock_daily_data(code, start_date, end_date=None):
     if not end_date:
         end_date = time_util.date_to_str()
     start_date = time_util.format_date(start_date)
     end_date = time_util.format_date(end_date)
     socket_code = global_operator.format_stock_code(code)
     df = global_operator.read(
-        "select trade_date,open,high,low,close,vol as volume,0 from daily where ts_code='{}' and trade_date>='{}' and trade_date<{}".format(
-            socket_code,start_date,end_date))
-    df.index = pd.to_datetime(df.trade_date)
+        "select trade_date,open,high,low,close,vol from daily where ts_code='{}' and trade_date>='{}' and trade_date<{}".format(
+            socket_code, start_date, end_date))
+    df['trade_date'] = pd.to_datetime(df['trade_date'])
     return df
-def plot_stock(code,title,start,end=None):
-    dd = get_stock_daily(code,start,end)
+
+
+def get_stock_daily_direct_data(code, start_date, end_date=None):
+    if not end_date:
+        end_date = time_util.date_to_str()
+    start_date = time_util.format_date(start_date)
+    end_date = time_util.format_date(end_date)
+    socket_code = global_operator.format_stock_code(code)
+    df = global_operator.read(
+        "select trade_date,open,high,low,close,vol from daily where ts_code='{}' and trade_date>='{}' and trade_date<{}".format(
+            socket_code, start_date, end_date))
+    df['trade_date'] = pd.to_datetime(df['trade_date'])
+    df.set_index('trade_date', inplace=True)
+    print(df)
+    return df
+
+
+def plot_stock(code, title, start, end=None):
+    dd = get_stock_daily_data(code, start, end)
     trade_date_count = len(dd.index)
-    print('trade_date_count:{}',trade_date_count)
-    dd.close.plot(figsize=(14,6),color='r')
-    plt.title(title+'价格走势\n'+start+':'+end,size=15)
-    plt.annotate(f'期间累计涨幅:{(dd.close[-1]/dd.close[0]-1)*100:.2f}%', xy=(dd.index[-trade_date_count//2],dd.close.mean()),
-             xytext=(dd.index[-trade_date_count//4],dd.close.min()), bbox = dict(boxstyle = 'round,pad=0.5',
-            fc = 'yellow', alpha = 0.5),
-             arrowprops=dict(facecolor='green', shrink=0.05),fontsize=12)
+    print('trade_date_count:{}', trade_date_count)
+    dd.close.plot(figsize=(14, 6), color='r')
+    plt.title(title + '价格走势\n' + start + ':' + end, size=15)
+    plt.annotate(f'期间累计涨幅:{(dd.close[-1] / dd.close[0] - 1) * 100:.2f}%',
+                 xy=(dd.index[-trade_date_count // 2], dd.close.mean()),
+                 xytext=(dd.index[-trade_date_count // 4], dd.close.min()), bbox=dict(boxstyle='round,pad=0.5',
+                                                                                      fc='yellow', alpha=0.5),
+                 arrowprops=dict(facecolor='green', shrink=0.05), fontsize=12)
     plt.show()
+
+
 if __name__ == '__main__':
-    plot_stock('比亚迪','比亚迪',start="20210101")
+    plot_stock('比亚迪', '比亚迪', start="20210101")
