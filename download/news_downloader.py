5bfc3f2d6b3652604b4a61cfd03765d99cf20010
ugeg <ugeg@outlook.com>
2022-12-31 20:18:49 +0800

    优化新闻下载

--- download/news_downloader.py
+++ download/news_downloader.py
@@ -32,7 +32,7 @@ def get_recent_news(end_date: str = datetime.datetime.now().strftime(time_format
 
 def loop_get_recent_news():
     start_date = '2021-01-01 00:00:00'
-    end_date = '2021-01-04 13:05:15'
+    end_date = datetime.datetime.now().strftime(time_format)
     while start_date < end_date:
         end_date = (datetime.datetime.strptime(end_date, time_format) - datetime.timedelta(seconds=1)).strftime(
             time_format)
@@ -41,5 +41,32 @@ def loop_get_recent_news():
         time.sleep(30)
 
 
+def get_news_js(forward=True):
+    if forward:
+        """从当前时间更新到表中最大时间"""
+        start_date = session.execute("select cast(max(datetime) as char) from news_jin10").first()[0]
+        end_date = datetime.datetime.now().strftime(time_format)
+    else:
+        """从表中最小时间更新到start_date"""
+        start_date = '2016-01-01 00:00:00'
+        end_date = session.execute("select cast(min(datetime) as char) from news_jin10").first()[0]
+    print('end_date:', end_date)
+    while start_date < end_date:
+        end_date = (datetime.datetime.strptime(end_date, time_format) - datetime.timedelta(seconds=1)).strftime(
+            time_format)
+        js_news_df = ak.js_news(timestamp=end_date)
+        js_news_df.dropna(inplace=True)
+        js_news_df = js_news_df[js_news_df['datetime'] > start_date]
+        js_news_df = js_news_df[js_news_df['content'] != '-']
+        js_news_df = js_news_df[js_news_df['content'] != '']
+        if len(js_news_df) == 0:
+            continue
+        save(js_news_df, "news_jin10")
+        end_date = js_news_df.iloc[0, 0]
+        print('end_date:', end_date)
+        # time.sleep(3)
+
+
 if __name__ == '__main__':
-    loop_get_recent_news()
+    # loop_get_recent_news()
+    get_news_js()
