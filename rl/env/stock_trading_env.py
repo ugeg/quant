2037789f2721cc846aa242398cb205759b21c266
ugeg <ugeg@outlook.com>
2024-09-23 17:24:09 +0800

    精简代码

--- rl/env/stock_trading_env.py
+++ rl/env/stock_trading_env.py
@@ -29,50 +29,35 @@ def transform(position: Positions, action: int) -> [Positions, bool]:
 
 
 class StockTradingEnv(gymnasium.Env):
-    metadata = {'render_modes': ['human']}
-    # metadata = {'render_mode': 'human'}
-    render_mode = 'human'
+    metadata = {'render_modes': ['human'],"render_fps":1}
     def __init__(self, raw_data: pd.DataFrame, window_size: int) -> None:
         self.trade_fee_ask_percent = 0.0015  # 卖出手续费+印花税
         self.trade_fee_bid_percent = 0.0005  # 买入手续费
-        self.train_range = None
-        self.test_range = None
         self.start_idx = 0
         self.end_idx = 0
         self._current_idx = None
         self._last_trade_tick = None
-        self._position = None
-        self._position_history = None
-        self._trade_history = None
-        self._total_reward = None
-        # 每次玩1年
-        self.episode_length = 253
-        trade_feature = ["is_hold"]
-        self.raw_close_prices = raw_data.loc[:, 'close'].to_numpy()  # 用于计算reward
+        self._position = Positions.FLAT
+        self._position_history = []
+        self._trade_history = []
+        self._total_reward = 0.
+        self.episode_length = 253  # 每次交易的天数
+        self.raw_close_prices = raw_data.loc[:, 'close'].to_numpy()  # 用于计算奖励
         self.df = raw_data
         self.window_size = window_size
+        # 定义动作空间和观察空间
         self.action_space = spaces.Discrete(len(Actions))
         self.observation_space = spaces.Box(low=-np.inf, high=np.inf,
-                                            shape=(window_size, raw_data.shape[1] + len(trade_feature)))
-        # 正态化
+                                            shape=(window_size, raw_data.shape[1] + 1))  # 加1是为了存储是否持仓的信息
+        self._profit_history = [1.] * self.window_size
+        # 数据标准化
         EPS = 1e-10
-        if self.train_range is None or self.test_range is None:
-            self.df = self.df.apply(lambda x: (x - x.mean()) / (x.std() + EPS), axis=0)
-        else:
-            boundary = int(len(self.df) * self.train_range)
-            train_data = raw_data[:boundary].copy()
-            boundary = int(len(raw_data) * (1 + self.test_range))
-            test_data = raw_data[boundary:].copy()
-
-            train_data = train_data.apply(lambda x: (x - x.mean()) / (x.std() + EPS), axis=0)
-            test_data = test_data.apply(lambda x: (x - x.mean()) / (x.std() + EPS), axis=0)
-            self.df.loc[train_data.index, train_data.columns] = train_data
-            self.df.loc[test_data.index, test_data.columns] = test_data
-        print("init finish")
+        self.df = self.df.apply(lambda x: (x - x.mean()) / (x.std() + EPS), axis=0)
+        print("初始化完成")
 
     def step(self, action: ActType) -> tuple[ObsType, SupportsFloat, bool, bool, dict[str, Any]]:
-        terminated = False  # 游戏结束
-        truncated = False  # 游戏超时
+        terminated = False  # 是否结束
+        truncated = False  # 是否超时
         self._current_idx += 1
         if self._current_idx >= self.end_idx:
             truncated = True
@@ -88,23 +73,17 @@ class StockTradingEnv(gymnasium.Env):
         self._position_history.append(self._position)
         self._profit_history.append(float(np.exp(self._total_reward)))
         observation = self._get_observation()
-        info = dict(
-            total_reward=self._total_reward,
-            position=self._position.value,
-        )
+        info = {
+            'total_reward': self._total_reward,
+            'position': self._position.value,
+        }
         return observation, step_reward, terminated, truncated, info
 
     def reset(self, *, seed: int | None = None, options: dict[str, Any] | None = None) -> tuple[
         ObsType, dict[str, Any]]:
         # return super().reset(seed=seed, options=options)
-        # 设置区间
-        if self.train_range is not None:
-            boundary = int(len(self.df) * self.train_range)
-            assert boundary - self.episode_length > self.window_size, \
-                "parameter test_range is too small!"
-            self.start_idx = np.random.randint(self.window_size, boundary - self.episode_length)
-        else:
-            self.start_idx = np.random.randint(self.window_size, len(self.df) - self.episode_length)
+        # 随机选择开始和结束索引
+        self.start_idx = np.random.randint(self.window_size, len(self.df) - self.episode_length)
         self.end_idx = self.start_idx + self.episode_length - 1
         self._current_idx = self.start_idx
         self._position = Positions.FLAT
@@ -117,14 +96,16 @@ class StockTradingEnv(gymnasium.Env):
     def render(self,mode='human') -> None:
         import matplotlib.pyplot as plt
         import matplotlib
+        matplotlib.rcParams['font.family'] = 'SimSun'  # 使用宋体
+        matplotlib.rcParams['axes.unicode_minus'] = False  # 解决负号 '-' 显示为方块的问题
         import datetime
-        matplotlib.use('TkAgg')
+        # matplotlib.use('TkAgg')
         plt.clf()
         # self.max_possible_profit()
         # plt.title("max_possible_profit:"+str(self.max_possible_profit())+"\nfinal_profit:"+str(self._profit_history[-1]))
-        plt.title("final_profit:"+str(self._profit_history[-1]))
-        plt.xlabel('trading days')
-        plt.ylabel('profit')
+        plt.title("最终利润:"+str(self._profit_history[-1]))
+        plt.xlabel('交易天数')
+        plt.ylabel('利润')
         plt.plot(self._profit_history)
         fig_dir = "./fig"
         import os
@@ -132,29 +113,15 @@ class StockTradingEnv(gymnasium.Env):
         plt.savefig(f"{fig_dir}/{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}-profit.png")
 
         plt.clf()
-        plt.xlabel('trading days')
-        plt.ylabel('close price')
-        window_ticks = np.arange(len(self._trade_history))
-        # eps_price = self.raw_close_prices[self.start_idx:self.end_idx + 1]
+        plt.xlabel('交易天数')
+        plt.ylabel('收盘价')
         eps_price = self.raw_close_prices[self.start_idx-self.window_size+1:self.end_idx + 1]
         plt.plot(eps_price,color='black', lw=2.)
-        short_ticks = []
-        long_ticks = []
-        flat_ticks = []
-        for i, tick in enumerate(window_ticks):
-            # if self._position_history[i] == Positions.SHORT:
-            #     short_ticks.append(tick)
-            # if self._position_history[i] == Positions.LONG:
-            if self._trade_history[i] == 1:
-                long_ticks.append(tick)
-            elif self._trade_history[i] == -1:
-                flat_ticks.append(tick)
-
-        # plt.plot(long_ticks, window_ticks[long_ticks], 'r^', markersize=4, label="Buy")
-        # plt.plot(flat_ticks, window_ticks[flat_ticks], 'gv', markersize=4, label="Sell")
-        plt.plot(long_ticks, eps_price[long_ticks], 'r^', markersize=4, label="Buy")
-        plt.plot(flat_ticks, eps_price[flat_ticks], 'gv', markersize=4, label="Sell")
-        # plt.plot(short_ticks, eps_price[short_ticks], 'rv', markersize=3, label="Short")
+        # 标记买入和卖出点
+        long_ticks = [i for i in range(len(self._trade_history)) if self._trade_history[i] == 1]
+        flat_ticks = [i for i in range(len(self._trade_history)) if self._trade_history[i] == -1]
+        plt.plot(long_ticks, eps_price[long_ticks], 'r^', markersize=4, label="买入")
+        plt.plot(flat_ticks, eps_price[flat_ticks], 'gv', markersize=4, label="卖出")
         plt.legend(loc='upper left', bbox_to_anchor=(0.05, 0.95))
         plt.savefig(f"./fig/{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}--price.png")
         plt.show()
@@ -176,6 +143,7 @@ class StockTradingEnv(gymnasium.Env):
             last_trade_price = (self.raw_close_prices[self._last_trade_tick])
             ratio = current_price / last_trade_price
             cost = (1 - self.trade_fee_ask_percent) * (1 - self.trade_fee_bid_percent)
+            # 计算卖出时的奖励
             step_reward = np.log(ratio * cost)
         else:
             # 资金闲置 沉没成本
